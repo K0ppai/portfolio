f945c6bf85ef18c19e055ba4a54ff99843e6f231
koppai <psthu.koppai@gmail.com>
2023-05-17 22:13:10 +0630

    Add dynamic html for project section

--- script.js
+++ script.js
@@ -11,31 +11,51 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   navMenu.classList.remove('active');
 }));
 
-const projectBtns = document.querySelectorAll('.seeproject');
 const body = document.querySelector('body');
-
+const btn = document.querySelectorAll('.seeproject');
 const data = [{
-  projectNum: '1',
+  projectNum: '0',
   name: 'Keeping track of hundreds of components',
   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum consequatur quae recusandae harum minus illum nisi reprehenderit maxime, modi, quam, nulla cumque! Incidunt, impedit doloribus?  a debitis quidem earum eum quibusdam quia, possimus soluta maiores eveniet quod et officia? Iusto minus provident veniam.',
   featuredImage: 'images/Snapshoot Portfolio.svg',
+  firstlineTitle: 'Multi-Post Stories',
+  secondlineTitle: 'Gain+Glory',
+  languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+  technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
+  linkLiveVersion: 'https://k0ppai.github.io/',
+  linkSource: 'https://github.com/K0ppai/portfolio-setup-mobile-1',
+},
+{
+  projectNum: '1',
+  name: 'Project2',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum consequatur quae recusandae harum minus illum nisi reprehenderit maxime, modi, quam, nulla cumque! Incidunt, impedit doloribus?  a debitis quidem earum eum quibusdam quia, possimus soluta maiores eveniet quod et officia? Iusto minus provident veniam.',
+  featuredImage: 'images/Snapshoot Portfolio.svg',
+  firstlineTitle: 'Multi-Post Stories',
+  secondlineTitle: 'Gain+Glory',
+  languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
   technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
   linkLiveVersion: 'https://k0ppai.github.io/',
   linkSource: 'https://github.com/K0ppai/portfolio-setup-mobile-1',
 },
 {
   projectNum: '2',
-  name: 'Project2',
+  name: 'project3',
   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum consequatur quae recusandae harum minus illum nisi reprehenderit maxime, modi, quam, nulla cumque! Incidunt, impedit doloribus?  a debitis quidem earum eum quibusdam quia, possimus soluta maiores eveniet quod et officia? Iusto minus provident veniam.',
   featuredImage: 'images/Snapshoot Portfolio.svg',
   technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
+  firstlineTitle: 'Multi-Post Stories',
+  secondlineTitle: 'Gain+Glory',
+  languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
   linkLiveVersion: 'https://k0ppai.github.io/',
   linkSource: 'https://github.com/K0ppai/portfolio-setup-mobile-1',
 },
 {
   projectNum: '3',
-  name: 'project3',
+  name: 'project4',
   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum consequatur quae recusandae harum minus illum nisi reprehenderit maxime, modi, quam, nulla cumque! Incidunt, impedit doloribus?  a debitis quidem earum eum quibusdam quia, possimus soluta maiores eveniet quod et officia? Iusto minus provident veniam.',
+  firstlineTitle: 'Multi-Post Stories',
+  secondlineTitle: 'Gain+Glory',
+  languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
   featuredImage: 'images/Snapshoot Portfolio.svg',
   technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
   linkLiveVersion: 'https://k0ppai.github.io/',
@@ -43,69 +63,64 @@ const data = [{
 },
 {
   projectNum: '4',
-  name: 'project4',
+  name: 'project5',
   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum consequatur quae recusandae harum minus illum nisi reprehenderit maxime, modi, quam, nulla cumque! Incidunt, impedit doloribus?  a debitis quidem earum eum quibusdam quia, possimus soluta maiores eveniet quod et officia? Iusto minus provident veniam.',
   featuredImage: 'images/Snapshoot Portfolio.svg',
   technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
+  firstlineTitle: 'Multi-Post Stories',
+  secondlineTitle: 'Gain+Glory',
+  languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
   linkLiveVersion: 'https://k0ppai.github.io/',
   linkSource: 'https://github.com/K0ppai/portfolio-setup-mobile-1',
 },
 {
   projectNum: '5',
-  name: 'project5',
-  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum consequatur quae recusandae harum minus illum nisi reprehenderit maxime, modi, quam, nulla cumque! Incidunt, impedit doloribus?  a debitis quidem earum eum quibusdam quia, possimus soluta maiores eveniet quod et officia? Iusto minus provident veniam.',
-  featuredImage: 'images/Snapshoot Portfolio.svg',
-  technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
-  linkLiveVersion: 'https://k0ppai.github.io/',
-  linkSource: 'https://github.com/K0ppai/portfolio-setup-mobile-1',
-},
-{
-  projectNum: '6',
   name: 'project6',
   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum consequatur quae recusandae harum minus illum nisi reprehenderit maxime, modi, quam, nulla cumque! Incidunt, impedit doloribus?  a debitis quidem earum eum quibusdam quia, possimus soluta maiores eveniet quod et officia? Iusto minus provident veniam.',
   featuredImage: 'images/Snapshoot Portfolio.svg',
+  firstlineTitle: 'Multi-Post Stories',
+  secondlineTitle: 'Gain+Glory',
+  languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
   technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
   linkLiveVersion: 'https://k0ppai.github.io/',
   linkSource: 'https://github.com/K0ppai/portfolio-setup-mobile-1',
 },
 ];
-const popUp = (n) => {
+const popUp = (i) => {
   const html = document.createElement('div');
-  const seeProjectNum = n.target.dataset.number;
   const mediaQuery = window.matchMedia('(max-width: 768px)');
-  const [dataForEach] = data.filter((element) => element.projectNum === seeProjectNum);
   if (mediaQuery.matches) {
-    dataForEach.featuredImage = 'images/popupmobile.svg';
-    dataForEach.technologies[0] = 'Ruby on Rails';
-    dataForEach.technologies[1] = 'CSS';
-    dataForEach.technologies[2] = 'JavaScript';
+    data[i].featuredImage = 'images/popupmobile.svg';
+    data[i].technologies[0] = 'Ruby on Rails';
+    data[i].technologies[1] = 'CSS';
+    data[i].technologies[2] = 'JavaScript';
   }
   html.innerHTML = `
   <div class="see-project-container active">
         <div class="close-container">
           <button type="button" class="close-button">&times;</button>
         </div>
-        <img src="${dataForEach.featuredImage}" id="project-img">
+        <img src="${data[i].featuredImage}" id="project-img">
         <div class="project-title-container">
-          <h2>${dataForEach.name}</h2>
+          <h2>${data[i].name}</h2>
           <div class="see-live-container">
-            <a href="${dataForEach.linkLiveVersion}">See live</a>
-            <a href="${dataForEach.linkLiveVersion}"><img src="images/See-live.svg"></a>
+            <a href="${data[i].linkLiveVersion}">See live</a>
+            <a href="${data[i].linkLiveVersion}"><img src="images/See-live.svg"></a>
           </div>
           <div class="see-source-container">
-            <a href="${dataForEach.linkSource}">See source</a>
-            <a href="${dataForEach.linkSource}"><img src="images/github-white.svg"></a>
+            <a href="${data[i].linkSource}">See source</a>
+            <a href="${data[i].linkSource}"><img src="images/github-white.svg"></a>
           </div>
         </div>
         <ul class="project-tech">
-          <li>${dataForEach.technologies[0]}</li>
-          <li>${dataForEach.technologies[1]}</li>
-          <li>${dataForEach.technologies[2]}</li>
-          <li>${dataForEach.technologies[3]}</li>
-          <li>${dataForEach.technologies[4]}</li>
-          <li>${dataForEach.technologies[5]}</li>
+          <li>${data[i].technologies[0]}</li>
+          <li>${data[i].technologies[1]}</li>
+          <li>${data[i].technologies[2]}</li>
+          <li>${data[i].technologies[3]}</li>
+          <li>${data[i].technologies[4]}</li>
+          <li>${data[i].technologies[5]}</li>
         </ul>
-        <p class="project-desc">${dataForEach.description}</p>
+        <p class="project-desc">${data[i].description}</p>
       </div>
       <div class="overlay active"></div>
   `;
@@ -116,6 +131,34 @@ const popUp = (n) => {
   });
 };
 
-projectBtns.forEach((button) => {
-  button.addEventListener('click', popUp);
-});
+function insertAfter(newNode, existingNode) {
+  existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
+const div = document.createElement('div');
+const parentElement = document.getElementById('portfolio');
+div.className = 'my-projects';
+div.innerHTML = '';
+insertAfter(div, parentElement.lastElementChild);
+for (let i = 0; i < 6; i += 1) {
+  div.innerHTML += `
+  <div id="project${i}" class="work-cards animation-left">
+        <div class="img-placeholder">
+        </div>
+        <div class="work-desc">
+          <p>${data[i].firstlineTitle}<br>${data[i].secondlineTitle}</p>
+          <div>
+            <ul class="languages">
+              <li>${data[i].languages[0]}</li>
+              <li>${data[i].languages[1]}</li>
+              <li>${data[i].languages[2]}</li>
+              <li>${data[i].languages[3]}</li>
+            </ul>
+          </div>
+          <div class="button-container">
+            <button type="button" onclick='popUp(${i})' class="button seeproject" id="seeproject${i}">See project</button>
+          </div>
+        </div>
+      </div>`;
+}
+btn.addEventListener('click', popUp());
