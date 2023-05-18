22b06cdbab16458cb5c572b952ea12390d9eec15
koppai <psthu.koppai@gmail.com>
2023-05-18 19:54:33 +0630

    Fix linter errors

--- js/formValidation.js
+++ js/formValidation.js
@@ -10,33 +10,3 @@ form.addEventListener('submit', (n) => {
     form.submit();
   }
 });
-
-// const btn = document.getElementById('get-in-touch');
-
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-// });
-// btn.addEventListener('click', () => {
-//   let email = document.getElementById('email');
-//   let fullname = document.getElementById('name');
-//   let textarea = document.getElementById('textarea');
-//   email = email.value;
-//   fullname = fullname.value;
-//   textarea = textarea.value;
-//   localStorage.setItem('name', fullname);
-//   localStorage.setItem('email', email);
-//   localStorage.setItem('message', textarea);
-//   email.value = localStorage.getItem('email');
-//   fullname.value = localStorage.getItem('name');
-//   textarea.value = localStorage.getItem('message');
-
-//   const inputData = {
-//     inputName: 'fullname',
-//     inputEmail: 'email',
-//     inputText: 'textarea',
-//   };
-//   const strInput = JSON.stringify(inputData);
-// });
-// console.log(strInput);
-// console.log(inputData)
-// localStorage.removeItem('data');
